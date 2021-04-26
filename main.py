a9537e2b8f901913219aeb53e393df282ca6f2f4
Samuel Trinder <20619907@student.curtin.edu.au>
2021-04-26 11:33:28 +0800

    added code

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 # code to take an input of binary and fli at a certain index
+thelist = [0]*64
 
+
+
+def flip(List, Index):
+    if List[Index] == 0:
+        List[Index] = 1
+    elif List[Index] == 1:
+        List[Index] = 0
+    else:
+        print('error')
+    return print(List)
+
+Index = int(input('enter bit to flip'))
+flip(thelist, Index)
